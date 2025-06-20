50d07ca2df952eb7bafe7d194591c2296cbebd8b
Gr Didalin <rewsbar@gmail.com>
2025-06-20 13:08:38 +0300

    fix error with types

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,7 +3,7 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
     host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT) || 7891,
+    port: parseInt(process.env.DB_PORT || '5432') || 7891,
     username: process.env.DB_USERNAME || 'postgres',
     password: process.env.DB_PASSWORD || 'postgres',
     database: process.env.DB_DATABASE || 'diplom',
