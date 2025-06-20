f4babf5f0bcd2d0b977e32eb82e1390a6842ed4f
Gr Didalin <rewsbar@gmail.com>
2025-06-20 13:06:44 +0300

    change type orm conf

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: 'localhost',
-    port: 7891,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'diplom',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT) || 7891,
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_DATABASE || 'diplom',
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true
 }
